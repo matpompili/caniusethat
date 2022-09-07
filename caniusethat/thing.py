ec5a80788240f2383480ba558f7f1f088cb37519
Matteo Pompili <matpompili@users.noreply.github.com>
2022-09-07 11:28:50 -0500

    simplify

--- caniusethat/thing.py
+++ caniusethat/thing.py
@@ -11,9 +11,28 @@ from caniusethat._types import (
     RemoteProcedureResponse,
     SharedMethodDescriptor,
 )
-from caniusethat._validate import _validate_rpc_response
 
 _logger = getLogger(__name__)
+
+
+def _validate_rpc_response(response: bytes) -> Any:
+    """Validates the response from the server.
+
+    Args:
+        response: The response from the server.
+
+    Returns:
+        The result of the RPC call, or raises an exception if the response is invalid.
+
+    Raises:
+        RuntimeError: If the response is invalid or if the response is an error."""
+    result = pickle.loads(response)
+    if not isinstance(result, RemoteProcedureResponse):
+        raise RuntimeError(f"Received invalid RemoteProcedureResponse: {result}")
+    if result.error != RemoteProcedureError.NO_ERROR:
+        raise RuntimeError(f"Remote procedure error: {result}")
+    else:
+        return result.result
 
 
 class Thing:
