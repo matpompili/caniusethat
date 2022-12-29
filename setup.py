a305617e382f1f4d22986de5a6a86a78928d794d
Matteo Pompili <matpompili@users.noreply.github.com>
2022-12-29 12:57:57 -0600

    Bump version: 0.4.1-beta.0 → 0.4.1

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ project_urls = {
 
 setup(
     name="caniusethat",
-    version="0.4.1-beta.0",
+    version="0.4.1",
     packages=["caniusethat"],
     author="Matteo Pompili",
     python_requires=">=3.8",
